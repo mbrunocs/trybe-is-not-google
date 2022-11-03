36f76681f1c0c478e47f870807851b2e8c04be54
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-11-03 12:17:21 -0300

    dev: Implemented process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+def process(path_file, instance: Queue):
+    data = txt_importer(path_file)
+    new_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    if not new_file in instance.list:
+        instance.enqueue(new_file)
+        sys.stdout.write(f'{new_file}')
 
 
 def remove(instance):
