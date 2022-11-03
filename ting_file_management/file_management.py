ee2fc7be1c14b487b5b56bbfbf1b408942f91e54
Marcio Bruno <m.bruno.cs@hotmail.com>
2022-11-03 11:34:22 -0300

    dev: Implemented txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação."""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, encoding="utf-8") as file:
+            file_reader = file.read()
+        return file_reader.split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
